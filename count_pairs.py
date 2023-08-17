d7ee12e630cfe3fb13a1ce533e05a4f0c7666550
xexxw <90753521+xexxw@users.noreply.github.com>
2023-08-17 16:32:59 +0800

    Add files via upload

---
+++ count_pairs.py
@@ -0,0 +1,16 @@
+import glob
+
+
+# txt_list = glob.glob("/home/pagevalue/lilinjun/slimpajama/new_test/nfcresult/dup_18gram/duplicate_pairs_filter*.txt")
+txt_list = glob.glob("/home/pagevalue/lilinjun/slimpajama/nfcresult/dup/duplicate_pairs*.txt")
+
+for file in txt_list:
+    # 读取txt文件并将内容添加到集合中
+    lines_set = set()
+    with open(file, 'r') as f:
+        for line in f:
+            lines_set.add(line.strip())
+
+# 输出不同内容行的数量
+unique_lines_count = len(lines_set)
+print("不同内容行的数量:", unique_lines_count)
