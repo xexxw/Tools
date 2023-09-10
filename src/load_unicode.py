ecb5ea03f6488122e589f616cbdbf9267fc1b916
xexxw <90753521+xexxw@users.noreply.github.com>
2023-09-10 16:49:07 +0800

    Update and rename load_unicode.py to src/load_unicode.py

--- src/load_unicode.py
+++ src/load_unicode.py
@@ -1,11 +1,13 @@
+import sys
+import os
 import codecs
 
 def decode_unicode_escape(input_string):
     return codecs.decode(input_string, 'unicode_escape')
 
-# 输入文件路径和输出文件路径
-input_file_path = 'input_file.txt'
-output_file_path = 'out_file.txt'
+# Derive the output file name from the input file name
+input_file_path = sys.argv[1]
+output_file_path = os.path.splitext(input_file_path)[0] + ".txt"
 
 with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
     for line in input_file:
